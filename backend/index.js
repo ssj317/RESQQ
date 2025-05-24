7c5f779cd5d4c0dc4b6cfd36bbc90349b0c03683
ssj317 <jenasaisubham8@gmail.com>
2025-05-24 19:51:04 +0530

    fixed database connection

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const Message = require("./models/message");
 const cors = require("cors");
+require('dotenv').config();
 
 // HTTP Server
 const server = require("http").createServer(app);
@@ -42,9 +43,7 @@ app.use((req, res, next) => {
   });
   
 
-// View Engine
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+
 
 // Routes
 app.use("/auth", routes);
@@ -53,12 +52,15 @@ let socketsConnected = new Set();
 
 // MongoDB Connection
 mongoose
-    .connect("mongodb://localhost:27017/RESQ")
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
-        console.log("MongoDB successfully connected");
+        console.log("✅ MongoDB connected successfully");
     })
     .catch((err) => {
-        console.error("MongoDB connection error:", err);
+        console.error("❌ MongoDB connection error:", err);
     });
 
 
