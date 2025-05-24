d50389335c7a339dd2fc304a32100640d4a64d35
Sai Subham Jena <jenasaisubham8@gmail.com>
2025-05-24 21:44:14 +0530

    Update NavSurvival.jsx

--- frontend/src/components/Navbar/NavSurvival.jsx
+++ frontend/src/components/Navbar/NavSurvival.jsx
@@ -10,7 +10,7 @@ const NavSurvival = () => {
       <div className='flex flex-row items-center justify-between p-4 px-12 border-white h-20 '>
         <div 
           className='h-28 w-28 mt-12 relative hover:scale-105 transition-transform duration-300 cursor-pointer'
-          onClick={() => navigate('https://resqq-frontend.onrender.com/')}
+          onClick={() => navigate('/')}
         >
           <img 
             src={logo} 
@@ -21,13 +21,13 @@ const NavSurvival = () => {
         
         <div className='text-white flex flex-row justify-between gap-4 mt-12 items-center'>
           <button 
-            onClick={() => navigate('https://resqq-frontend.onrender.com/signup')}
+            onClick={() => navigate('/signup')}
             className='border-2 border-blue-900/60 text-black bg-transparent hover:bg-blue-500/30 hover:scale-95 px-6 py-2 rounded-lg cursor-pointer transition-all duration-300 text-sm font-medium '
           >
             Sign Up
           </button>
           <button 
-            onClick={() => navigate('https://resqq-frontend.onrender.com/login')}
+            onClick={() => navigate('/login')}
             className='border-2 border-blue-900/60 text-black bg-white/10 hover:bg-blue-500/30 hover:scale-95 px-6 py-2 rounded-lg cursor-pointer transition-all duration-300 text-sm font-medium'
           >
             Login
