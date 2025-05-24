7b352165e1f2e5f5580e1807b903f74aeb6438e7
Sai Subham Jena <jenasaisubham8@gmail.com>
2025-05-24 22:40:30 +0530

    Update auth_route.js

--- backend/routes/auth_route.js
+++ backend/routes/auth_route.js
@@ -362,7 +362,13 @@ router.get('/api/disaster-data', async (req, res) => {
     console.log('Scraping disaster data...');
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+     args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--single-process',
+        '--no-zygote',
+      ],,
       defaultViewport: null,
     });
 
